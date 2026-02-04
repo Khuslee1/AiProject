171ca4fa1b131e35ed21395215339246b5620ce2
Khuslee1 <Khuselee11@gmail.com>
2026-02-04 12:42:03 +0800

    hariad hiine

--- app/components/ImageAnalysis.tsx
+++ app/components/ImageAnalysis.tsx
@@ -1,10 +1,15 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 import { BsFileEarmarkText } from "react-icons/bs";
 import { IoRefresh } from "react-icons/io5";
 import { PiStarFourBold } from "react-icons/pi";
+import { RiDeleteBin7Line } from "react-icons/ri";
 
 export const ImageAnalysis = () => {
+  const [preview, setPreview] = useState<string | null>(null);
+
   return (
     <>
       <div className="flex justify-between">
@@ -19,10 +24,37 @@ export const ImageAnalysis = () => {
       <div className="text-sm text-[#71717A]">
         Upload a food photo, and AI will detect the ingredients.
       </div>
-      <Input />
+      <Input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files?.[0];
+          if (file) {
+            setPreview(URL.createObjectURL(file));
+          }
+        }}
+      />
       <div className="w-full flex justify-end">
         <Button>Generate</Button>
       </div>
+      {preview && (
+        <div className="relative w-40 h-40">
+          <img
+            src={preview}
+            alt="preview"
+            className="w-40 h-40 object-cover rounded-lg"
+          />
+          <Button
+            size="icon"
+            variant={"outline"}
+            className="absolute bottom-2 right-2"
+            onClick={() => {
+              setPreview(null);
+            }}
+          >
+            <RiDeleteBin7Line />
+          </Button>
+        </div>
+      )}
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2">
           <div className="flex gap-2 items-center">
