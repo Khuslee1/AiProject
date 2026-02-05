b3d132bbb47d7fb5df020a0a34af303b2f4b9ac9
Khuslee1 <Khuselee11@gmail.com>
2026-02-05 12:38:16 +0800

    mar bolido

--- app/components/ImageAnalysis.tsx
+++ app/components/ImageAnalysis.tsx
@@ -1,14 +1,76 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { BsFileEarmarkText } from "react-icons/bs";
 import { IoRefresh } from "react-icons/io5";
 import { PiStarFourBold } from "react-icons/pi";
 import { RiDeleteBin7Line } from "react-icons/ri";
+import { pipeline } from "@huggingface/transformers";
 
 export const ImageAnalysis = () => {
   const [preview, setPreview] = useState<string | null>(null);
+  const captionref = useRef<any>(null);
+  const ingredientRef = useRef<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<string | null>(null);
+
+  const HandleGenerate = async () => {
+    setLoading(true);
+
+    try {
+      if (!captionref.current) {
+        captionref.current = await pipeline(
+          "image-classification",
+          // "Xenova/vit-gpt2-image-captioning",
+          "Xenova/vit-base-patch16-224",
+          { device: "webgpu" },
+        );
+      }
+
+      const captionOutput = await captionref.current(preview);
+
+      console.log(captionOutput, "captionOutput");
+
+      const caption = Array.isArray(captionOutput)
+        ? captionOutput.filter((ele) => ele.score > 0.5)?.push()
+        : (captionOutput as { generated_text: string })?.generated_text;
+
+      if (!ingredientRef.current) {
+        ingredientRef.current = await pipeline(
+          "text-generation",
+          "HuggingFaceTB/SmolLM2-135M-Instruct",
+        );
+      }
+
+      const ingredientPrompt = `
+      Extract a unique list of ingredients from the following food description.
+      Do not repeat ingredients. Return a comma-separated list.
+
+      Description:
+      ${caption}
+      `;
+      const messages = [
+        { role: "system", content: "You are helpful assistant" },
+        { role: "user", content: ingredientPrompt },
+      ];
+
+      const ingredientOutput = await ingredientRef.current(messages, {
+        max_new_tokens: 80,
+      });
+
+      const ingredients = Array.isArray(ingredientOutput)
+        ? ingredientOutput[2]?.content
+        : ingredientOutput?.content;
+
+      setResult(ingredients);
+      console.log(ingredients);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -17,7 +79,14 @@ export const ImageAnalysis = () => {
           <PiStarFourBold />
           <h1 className="font-semibold text-xl">Image analysis</h1>
         </div>
-        <Button size="icon" variant={"outline"}>
+        <Button
+          size="icon"
+          variant={"outline"}
+          onClick={() => {
+            setPreview(null);
+            setResult(null);
+          }}
+        >
           <IoRefresh />
         </Button>
       </div>
@@ -26,6 +95,7 @@ export const ImageAnalysis = () => {
       </div>
       <Input
         type="file"
+        placeholder="JPG or PNG"
         onChange={(e) => {
           const file = e.target.files?.[0];
           if (file) {
@@ -34,7 +104,7 @@ export const ImageAnalysis = () => {
         }}
       />
       <div className="w-full flex justify-end">
-        <Button>Generate</Button>
+        <Button onClick={() => HandleGenerate()}>Generate</Button>
       </div>
       {preview && (
         <div className="relative w-40 h-40">
@@ -49,6 +119,7 @@ export const ImageAnalysis = () => {
             className="absolute bottom-2 right-2"
             onClick={() => {
               setPreview(null);
+              setResult(null);
             }}
           >
             <RiDeleteBin7Line />
@@ -61,8 +132,14 @@ export const ImageAnalysis = () => {
             <BsFileEarmarkText />
             <h1 className="font-semibold text-xl">Here is the summary</h1>
           </div>
-          <div className="text-sm text-[#71717A]">
-            Upload a food photo, and AI will detect the ingredients.
+          <div
+            className={`text-sm ${result ? "text-black" : "text-[#71717A]"} p-2 ${preview && "border border-[#E4E4E7] rounded-md"}`}
+          >
+            {result && !loading
+              ? result
+              : loading
+                ? "Working..."
+                : "Upload a food photo, and AI will detect the ingredients."}
           </div>
         </div>
       </div>
--- app/components/Ingredients.tsx
+++ app/components/Ingredients.tsx
@@ -3,8 +3,55 @@ import { Textarea } from "@/components/ui/textarea";
 import { BsFileEarmarkText } from "react-icons/bs";
 import { IoRefresh } from "react-icons/io5";
 import { PiStarFourBold } from "react-icons/pi";
+import { pipeline } from "@huggingface/transformers";
+import { useRef, useState } from "react";
 
 export const Ingredient = () => {
+  const captionref = useRef<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<string>("");
+  const [preview, setPreview] = useState<string>("");
+  const Handlecaptioning = async () => {
+    setLoading(true);
+    setResult("");
+    try {
+      if (!captionref.current) {
+        captionref.current = await pipeline(
+          "text-generation",
+          "HuggingFaceTB/SmolLM2-135M-Instruct",
+        );
+      }
+
+      const prompt = `
+You are a helpful assistant that extracts ingredients from food descriptions. 
+Read the description below and return a list of unique ingredients. 
+Do not include any duplicates. 
+
+Return only the ingredients as a comma-separated list.
+
+Food description:
+${preview}
+`;
+      const messages = [
+        { role: "system", content: "You are helpful assistant" },
+        { role: "user", content: prompt },
+      ];
+      const output = await captionref.current(messages, {
+        max_new_tokens: 1000,
+      });
+
+      const text = Array.isArray(output)
+        ? output[0]?.generated_text
+        : output.generated_text;
+
+      setResult(text);
+      console.log(text);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
       <div className="flex justify-between">
@@ -12,7 +59,14 @@ export const Ingredient = () => {
           <PiStarFourBold />
           <h1 className="font-semibold text-xl">Ingredient recognition</h1>
         </div>
-        <Button size="icon" variant={"outline"}>
+        <Button
+          size="icon"
+          variant={"outline"}
+          onClick={() => {
+            setPreview("");
+            setResult("");
+          }}
+        >
           <IoRefresh />
         </Button>
       </div>
@@ -20,9 +74,20 @@ export const Ingredient = () => {
         <div className="text-sm text-[#71717A]">
           Describe the food, and AI will detect the ingredients.
         </div>
-        <Textarea />
+        <Textarea
+          placeholder="Орц тодорхойлох"
+          onChange={(e) => {
+            setPreview(e.target.value);
+          }}
+        />
         <div className="w-full flex justify-end">
-          <Button>Generate</Button>
+          <Button
+            onClick={() => {
+              Handlecaptioning();
+            }}
+          >
+            Generate
+          </Button>
         </div>
       </div>
       <div className="flex flex-col gap-2">
@@ -31,8 +96,17 @@ export const Ingredient = () => {
             <BsFileEarmarkText />
             <h1 className="font-semibold text-xl">Identified Ingredients</h1>
           </div>
-          <div className="text-sm text-[#71717A]">
+          {/* <div className="text-sm text-[#71717A]">
             First, enter your text to recognize an ingredients.
+          </div> */}
+          <div
+            className={`text-sm ${result ? "text-black" : "text-[#71717A]"} p-2 ${preview && "border border-[#E4E4E7] rounded-md"}`}
+          >
+            {result && !loading
+              ? result
+              : loading
+                ? "Working..."
+                : "First, enter your text to recognize an ingredients."}
           </div>
         </div>
       </div>
